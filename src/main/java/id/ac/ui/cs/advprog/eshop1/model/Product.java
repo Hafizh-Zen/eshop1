160824a771bf0feb5aea00994c118dba7aa6ca4e
Hafizh Zen <hafizh.suryamustafa@gmail.com>
2025-03-07 00:30:55 +0700

    Product.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop1/model/Product.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.eshop1.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Product {
+    private int productId;
+    private String productName;
+    private int productQuantity;
+}
