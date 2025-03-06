de629bd7cc60216819be7a2d942329a497ae84a9
Hafizh Zen <hafizh.suryamustafa@gmail.com>
2025-03-06 16:42:58 +0700

    extra initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop1/Eshop1Application.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eshop1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eshop1Application.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop1/Eshop1ApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eshop1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
